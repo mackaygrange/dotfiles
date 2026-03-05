8ed33cb0c1fd343f8fdb4406047edfcf31fae633
Mackay Grange <mgrange@arkhamtechnology.com>
2026-03-05 11:42:18 -0700

    tmux ricing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -98,7 +98,6 @@ declare -a config_list=(
 # Home directory files: source_file|destination
 declare -a home_files=(
     "bash/.bashrc|$HOME/.bashrc"
-    "bash/.bash_profile|$HOME/.bash_profile"
     "bash/.inputrc|$HOME/.inputrc"
     "user-dirs.dirs|$CONFIG_DIR/user-dirs.dirs"
     "tmux/.tmux.conf|$HOME/.tmux.conf"
