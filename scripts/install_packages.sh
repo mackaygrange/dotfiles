e26fd8b53130f4b1d632c9947df2925239204755
Mackay Grange <mackay.grange@gmail.com>
2026-04-03 14:05:11 -0600

    Added automation of npm and nvm installation to setup script.

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -34,65 +34,94 @@ detect_os_and_distro() {
 
 # Install zoxide:
 install_zoxide() {
-  echo ""
-  echo "[*] Installing zoxide from cargo..."
-  if command -v cargo &>/dev/null 2>&1; then
-    if command -v zoxide &>/dev/null 2>&1; then
-      echo "[-] Zoxide is already installed!" 
-    else  
-      cargo install zoxide
-    echo "[+] Installed zoxide!"
-    fi
-  else
-    echo "[!] Cargo / Rustup not installed! Skipping install."
-  fi
+	echo ""
+	echo "[*] Installing zoxide from cargo..."
+	if command -v cargo &>/dev/null 2>&1; then
+		if command -v zoxide &>/dev/null 2>&1; then
+			echo "[-] Zoxide is already installed!"
+		else
+			cargo install zoxide
+			echo "[+] Installed zoxide!"
+		fi
+	else
+		echo "[!] Cargo / Rustup not installed! Skipping install."
+	fi
 }
 
-# Install tpm: 
+# Install tpm:
 install_tpm() {
-  echo ""
-  echo "[*] Installing tmux package manager from github..."
-  if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo "[+] Installed tmux package manager!"
-  else 
-    echo "[-] Tmux package manager already installed!"
-  fi
+	echo ""
+	echo "[*] Installing tmux package manager from github..."
+	if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+		echo "[+] Installed tmux package manager!"
+	else
+		echo "[-] Tmux package manager already installed!"
+	fi
+}
+
+install_nvm() {
+	echo ""
+	echo "[*] Installing node version manager from github..."
+	if command -v curl &>/dev/null 2>&1; then
+		if command -v nvm &>/dev/null 2>&1; then
+			echo "[-] Node version manager is already installed!"
+		else
+			curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+			echo "[+] Installed node version manager!"
+		fi
+	else
+		echo "[!] Curl not installed! Skipping install."
+	fi
+
+
+install_npm() {
+	echo ""
+	echo "[*] Installing node package manager with node version manager..."
+	if command -v nvm &>/dev/null 2>&1; then
+		if command -v npm &>/dev/null 2>&1; then
+			echo "[-] Node package manager is already installed!"
+		else
+			nvm install --lts
+			echo "[+] Installed node package manager!"
+		fi
+	else
+		echo "[!] Node version manager not installed! Skipping install."
+	fi
 }
 
 # Array containing the apt packages to download for ubuntu:
 declare -a ubuntu_apt_packages=(
-  "ssh"
-  "git"
-  "curl"
-  "vim"
-  "minicom"
-  "wireshark"
-  "tshark"
-  "make"
-  "cmake"
-  "gcc"
-  "g++"
-  "python"
-  "python-is-python3"
-  "python-pip"
-  "rustup"
-  "cargo"
-  "npm"
-  "lua5.1"
-  "luarocks"
-  "snap"
-  "hsetroot"
-  "xclip"
-  "scrot"
-  "i3"
-  "i3-status"
-  "picom"
-  "rofi"
-  "kitty"
-  "neofetch"
-  "lsd"
-  "tmux"
+	"ssh"
+	"git"
+	"curl"
+	"vim"
+	"minicom"
+	"wireshark"
+	"tshark"
+	"make"
+	"cmake"
+	"gcc"
+	"g++"
+	"python"
+	"python-is-python3"
+	"python-pip"
+	"rustup"
+	"cargo"
+	"lua5.1"
+	"luarocks"
+	"snap"
+	"hsetroot"
+	"xclip"
+	"scrot"
+	"i3"
+	"i3-status"
+	"picom"
+	"rofi"
+	"kitty"
+	"neofetch"
+	"lsd"
+	"tmux"
 )
 
 # Array containing the snap packeges to download for ubuntu:
@@ -109,7 +138,7 @@ declare -a arch_pacman_packages=(
 install_ubuntu_packages() {
   echo ""
   echo "[*] Installing Ubuntu packages..."
-  
+
   for pkg in "${ubuntu_apt_packages[@]}"; do
     sudo apt-get install -y "$pkg"
   done
@@ -164,22 +193,24 @@ echo ""
 if [ $EUID -ne 0 ]; then
   if [ "$INSTALL_FLAG" == 1  ]; then
     echo "[*] Installing packages..."
-    if [ "$DISTRO" == "ubuntu" ]; then 
+    if [ "$DISTRO" == "ubuntu" ]; then
       install_ubuntu_packages
-    elif [ "$DISTRO" == "arch" ]; then 
+    elif [ "$DISTRO" == "arch" ]; then
       install_arch_packages
-    else 
-      echo "[!] $DISTRO distribution is not yet supported!" 
+    else
+      echo "[!] $DISTRO distribution is not yet supported!"
     fi
-  fi 
+  fi
 else
   echo "[!] Do not run this script with sudo privileges, unexpected behaviour will occur"
   exit 1
 fi
 
 # These can be installed regardless of distro:
-install_zoxide 
+install_zoxide
 install_tpm
+install_nvm
+install_npm
 
 echo ""
 echo "[OK] Package installation complete!"
